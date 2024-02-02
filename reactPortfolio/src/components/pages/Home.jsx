f6ed3832d9f16f9b3a67d9c1a01c210b01d95ce0
sebecjeanluc <tkawamura11@gmail.com>
2024-02-02 23:50:39 +0000

    fixed the image path

--- reactPortfolio/src/components/pages/Home.jsx
+++ reactPortfolio/src/components/pages/Home.jsx
@@ -6,13 +6,15 @@ function Home(){
   return (
     <section id="home">
       <div className="home__wrapper">
-        <img src="./src/assets/images/pf_pic_BL.jpg" alt="Tak profile picture"/>
-        <div className="home-title__wrapper">
+        <div className="home_profile_pic--wrapper">
+          <img src="/reactPortfolio/src/assets/images/pf_pic_BL.jpg" alt="Tak profile picture"/>
+        </div>
+        <div className="home_title--wrapper">
           <h3>TAK -uya- KAWAMURA's PORTFOLIO</h3>
           <h4>IT's ALL ABOUT STORY</h4>
           <p>Takuya Kawamura, a front-end developer with a keen eye for film and interactive media, has made a significant transition from admiring movie storytelling to mastering the digital realm. Recognizing the untapped potential of smartphones, Takuya co-founded a web service dedicated to innovative collaborative storytelling. His expertise lies in crafting user interfaces that seamlessly integrate aesthetics with functionality, aiming to revolutionize digital experiences through engaging narratives and interactive storytelling techniques.</p>
           <div className="contact-button__wrapper">
-            <a href="../src/assets/pdf/TakuyKawamuraResume2023EN-technicalResume.pdf" download>
+            <a href="/reactPortfolio/src/assets/pdf/TakuyKawamuraResume2023EN-technicalResume.pdf" download>
           <button className="contact-button">RESUME DOWNLOAD</button></a>
           <div className="contact-button__wrapper">
             EMAIL: tkawamura11@gmail.com
@@ -21,11 +23,13 @@ function Home(){
         </div>
       </div>
       <div className="subSection__wrapper">
-        <div className="contact-button__wrapper">
-          <button className="contact-button">
-          <NavLink to='/contact'>CONTACT HIM</NavLink>
+        <NavLink to='/contact'>
+          <div className="contact-button__wrapper">
+            <button className="contact-button">
+              CONTACT HIM
             </button>
-        </div>
+          </div>
+        </NavLink>
       </div>
       <Project />
     </section>
